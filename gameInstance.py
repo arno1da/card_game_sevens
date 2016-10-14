3784f3f0dbfaf6b79b2af6140ab435639528404a
Arnold Agus <Arnold Agus>
2016-10-14 22:14:01 +1100

    adjusted git ingore

--- gameInstance.py
+++ gameInstance.py
@@ -3,12 +3,15 @@ import itertools
 
 # hand = random.sample(self.deck, 5)
 
+#Start simulation with two players
+
 class newDeck(object):
-	def __init__(self, players):
+	def __init__(self, players, strategyDict):
 		self.players = players
 		self.suits = 'cdhs'
 		self.ranks = '23456789TJQKA'
 		self.deck = tuple(''.join(card) for card in itertools.product(self.ranks, self.suits))
+
 		self.rounds = [
 		  {
 		  	"cards": 1
@@ -26,58 +29,47 @@ class newDeck(object):
 			, "name" : 'third round'
 
 		  }
-		  , {
-			"cards": 4
-			, "trick": 'faceup'
-			, "name": 'fourth round'
-
-		  }
-		  , {
-			"cards": 5
-			, "trick": 'faceup'
-			, "name" : 'fifth round'
-
-		  }
-		  , {
-			"cards": 6
-			, "trick": 'faceup'
-			, "name" : 'sixth round'
-
-		  }
-		  , {
-			"cards": 7
-			, "trick": 'faceup'
-			, "name": 'seventh round'
-
-		  }
-		  , {
-			"cards": 7
-			, "trick": 'None'
-			, "name": 'No trick round'
-
-		  }
-		  , {
-			"cards": 7
-			, "trick": 'faceDown'
-			, "name": 'Face down trick round'
-
-		  }
-		  , {
-			"cards": 7
-			, "trick": 'allBlind'
-			, "name" : "blind betting round"
-
-		  }
 		]
 		pass
 
-	def deal(cards, trick=None):
+	def drawCards(self, cards):
+		return random.sample(self.deck, cards)
+
+	def popCards(self, cardsPerHand, cardSample):
+		hand = []
+		for i in range(cardsPerHand):
+			hand.append(cardSample.pop())
+		return hand
+
+	def dealHand(self, gameRound, players):
+		
+		boardState = {}
+		if (gameRound['trick'] == 'faceup'):
+			cardSample = drawCards(gameRound['cards'] * int(players) + 1)
+			boardState['trick'] = cardSample.pop()
+			for i in range(players):
+				boardState['player' + str(i)] = self.popCards(gameRound['cards'], cardSample)
+		else:
+			drawCards(gameRound['cards'] * int(players))
+			for i in range(players):
+				boardState['player' + str(i)] = self.popCards(gameRound['cards'], cardSample)
+
+		return boardState
+
+	'''During the bidding round each player can bid as much as possible
+	however the final player can not bid such that the sum of the previous
+	bids is equal to amount of cards in each players hand. (note all players will have
+	the same amount of cards)
+	'''
+	def biddingRound(self, choices, cards):
 		pass
 
-	def runIteration(position, playerTree):
-		pass
+	def runGame(self, roundsLeft, players, scoreCard):
+		if len(roundsLeft) == 0:
+			return scoreCard
+		gameRound = roundsLeft.pop(0)
 
-	def runGame(self, roundsLeft, players):
+
 		pass
 
 if __name__ == "__main__":
